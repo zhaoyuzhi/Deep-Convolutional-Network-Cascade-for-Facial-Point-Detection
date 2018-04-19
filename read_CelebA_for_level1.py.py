220813a3b18ddb110e496fc21ce3e138cfa73aa5
Yuzhi_ZHAO <531708015@qq.com>
2018-04-19 17:56:19 +0800

    Add files via upload

---
+++ read_CelebA_for_level1.py.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 09:37:38 2018
+
+@author: zhaoyuzhi
+"""
+
+#This py file is to process CelebA images to train the networks
+#CelebA image lib has 202599 images with 5 key points label
+#I think CelebA could be regarded as training images and LFPW/BioID could be regarded as testing images
+from PIL import Image
+import math
+import xlrd
+
+#It is important that before you use this code, you should modify the image URLs
+IMAGEURL = "G:\\CelebA\\Img\\img_align_celeba"
+
+IMAGESAVEURL_F1_CelebA = "G:\\CelebA\\Img\\img_align_celeba_F1"
+IMAGESAVEURL_EN1_CelebA = "G:\\CelebA\\Img\\img_align_celeba_EN1"
+IMAGESAVEURL_NM1_CelebA = "G:\\CelebA\\Img\\img_align_celeba_NM1"
+
+bbox_excel = xlrd.open_workbook('CelebA_faceboundingbox.xlsx')
+bbox_table = bbox_excel.sheet_by_index(0)
+#landmarks_excel = xlrd.open_workbook('CelebA_align_landmarks.xlsx')
+#landmarks_table = landmarks_excel.sheet_by_index(0)
+
+#this is for CelebA
+for i in range(204599):
+    imagename = bbox_table.cell(i+1,0).value
+    img = Image.open(IMAGEURL + '\\' + imagename,'r')
+    #input images (width = 178, height = 218)
+    #output/crop face_bounding_box(left,top,right,bottom)
+    #img.resize((width, height))
+    #at first crop a square region for test, length is 89
+    #I choose the coordinate of left_top corner as (44,90) and right_bottom corner as (133,179)
+    #that is faceboundingbox = [44,90,133,179]
+    #for F1:
+    faceboundingbox_F1 = [44,90,133,179]
+    region = img.crop(faceboundingbox_F1)   #face bounding box image
+    region_resize = region.resize((39,39), Image.ANTIALIAS)   #F1 image
+    region_resize_grey = region_resize.convert('L')   #convert to grey image
+    region_resize_grey.save(IMAGESAVEURL_F1_CelebA + '\\' + imagename)
+    #for EN1:
+    faceboundingbox_EN1 = [50,90,127,150]
+    region_EN1 = img.crop(faceboundingbox_EN1)
+    region_EN1_resize = region_EN1.resize((39,31), Image.ANTIALIAS)   #EN1 image
+    region_EN1_resize_grey = region_EN1_resize.convert('L')   #convert to grey image
+    region_EN1_resize_grey.save(IMAGESAVEURL_EN1_CelebA + '\\' + imagename)
+    #for NM1:
+    faceboundingbox_NM1 = [50,119,127,179]
+    region_NM1 = img.crop(faceboundingbox_NM1)
+    region_NM1_resize = region_NM1.resize((39,31), Image.ANTIALIAS)   #EN1 image
+    region_NM1_resize_grey = region_NM1_resize.convert('L')   #convert to grey image
+    region_NM1_resize_grey.save(IMAGESAVEURL_NM1_CelebA + '\\' + imagename)
