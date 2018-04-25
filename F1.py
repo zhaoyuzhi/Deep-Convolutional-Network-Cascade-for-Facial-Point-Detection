e5792ecc4d59c8c50a8c5ac62e42c9168fa5a0a5
Yuzhi_ZHAO <531708015@qq.com>
2018-04-25 16:26:16 +0800

    Update F1.py

--- F1.py
+++ F1.py
@@ -169,15 +169,15 @@ a_conv4 = tf.nn.relu(h_conv4)                                       #outsize = b
 x_flat = tf.reshape(a_conv4, [-1,320])                              #outsize = batch*320
 
 ## fully connected layer 1
-W_fc1 = nl.weight_variable([320,120])
-b_fc1 = nl.bias_variable([120])
+W_fc1 = weight_variable([320,120])
+b_fc1 = bias_variable([120])
 h_fc1 = tf.matmul(x_flat, W_fc1) + b_fc1                            #outsize = batch*120
 a_fc1 = tf.nn.relu(h_fc1)                                           #outsize = batch*120
 a_fc1_dropout = tf.nn.dropout(a_fc1, keep_prob)                     #dropout layer 1
 
 ## fully connected layer 2
-W_fc2 = nl.weight_variable([120,10])
-b_fc2 = nl.bias_variable([10])
+W_fc2 = weight_variable([120,10])
+b_fc2 = bias_variable([10])
 h_fc2 = tf.matmul(a_fc1_dropout, W_fc2) + b_fc2                     #outsize = batch*10
 a_fc2 = tf.nn.relu(h_fc2)                                           #outsize = batch*10
 
