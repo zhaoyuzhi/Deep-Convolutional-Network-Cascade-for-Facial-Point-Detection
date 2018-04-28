dcd41f2e981d1fc3b9398d375fe28f2346913a9a
Yuzhi_ZHAO <531708015@qq.com>
2018-04-28 09:34:45 +0800

    Update EN1.py

--- EN1.py
+++ EN1.py
@@ -191,14 +191,22 @@ init = tf.global_variables_initializer()
 #average accuracy every batch
 accuracy = tf.reduce_mean((y - EN1_a_fc2), 0)                       #average accuracy every batch
 testaccuracy = np.zeros([27,6], dtype = np.float32)
+#save the model
+saver = tf.train.Saver()
 
 with tf.Session() as sess:
     sess.run(init)
-    for i in range(500):                                            #number of iterations:500*625=312500, 5 millon images
+    for i in range(5001):                                           #number of iterations:500*625=312500, 5 millon images
         for m in range(625):                                        #training process using training data 10000 images
             train_xbatch = x_data[(m*16):(m*16+16),:,:]             #train 16 data every batch, not including m*16+16
             train_ybatch = y_data[(m*16):(m*16+16),:]               #train 16 data every batch, not including m*16+16
             sess.run(Optimizer, feed_dict = {x:train_xbatch, y:train_ybatch, keep_prob:0.5})
+            if m % 125 == 0:
+                iteration = i * 625 + m
+                print('The iteration number is:',iteration)
+                print('The loss is:',sess.run(original_cost, feed_dict = {x:train_xbatch, y:train_ybatch, keep_prob:1}))
+        if i % 50 == 0:
+            save_path = saver.save(sess, "EN1_net/save_net.ckpt")   #save the model
         
     for n in range(27):
         test_xbatch = x_test[(n*128):(n*128+128),:,:]               #train 100 data every time, not including m*100+100
